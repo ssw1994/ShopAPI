aef2f0afe2c9903739433973acc33b0ca77a3b85
ssw1994 <waghmare.sachin1994@gmail.com>
2018-11-16 13:16:30 +0530

    added multer and cors

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var productsRouter = require("./routes/products");
 
 var app = express();
 
+var corsOptions = {
+  origin: 'http://localhost',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+app.use(cors(corsOptions))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var multer  =   require('multer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var storage =   multer.diskStorage({  
+  destination: function (req, file, callback) {  
+    callback(null, './uploads');  
+  },  
+  filename: function (req, file, callback) {  
+    callback(null, file.originalname);  
+  }  
+});
+
+var upload = multer({ storage : storage}).single('myfile');
+
+router.post('/upload',function(req,res){
+  upload(req,res,function(err) {  
+      if(err) {  
+          return res.end("Error uploading file.");  
+      }  
+      res.end(JSON.stringify({fileName:req.file.originalname,status:"File is uploaded successfully!"}));
+  });  
+});  
+
+
 
 module.exports = router;
