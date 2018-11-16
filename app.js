63f3d12e6c5ce7ab6d0ba6f2c2058ed4ab1f3e9d
ssw1994 <waghmare.sachin1994@gmail.com>
2018-11-16 13:24:09 +0530

    updated cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,13 +35,6 @@ app.use("/products",productsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 // error handler
